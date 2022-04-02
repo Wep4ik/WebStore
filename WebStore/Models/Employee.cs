f17e1b4b4988485668463e57eef9b65ec62897cf
Andrian <andrianpirlici@gmail.com>
2022-04-02 10:16:55 +0300

    Employee model.

---
+++ WebStore/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace WebStore.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PatronimicName { get; set; }
+        public string DepartamentName { get; set; }
+        public DateTime Birthday { get; set; }
+        public int Age { get; set; }
+    }
+}
