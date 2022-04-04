6de6fc3928a9d80d2cfa1be3e68fc9554a060a66
Andrian <andrianpirlici@gmail.com>
2022-04-04 23:58:16 +0300

    Employees controller with detail and index views.

---
+++ WebStore/Controllers/EmployeesController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers;
+
+public class EmployeesController : Controller
+{
+    private static readonly List<Employee> _employees = new()
+    {
+        new Employee()
+        {
+            Age = 23,
+            Birthday = new DateTime(1991, 05, 20),
+            DepartamentName = "Vanzari",
+            FirstName = "Aurel",
+            LastName = "Cavaz",
+            Id = 1,
+            PatronimicName = "Joricovici"
+        },
+        new Employee()
+        {
+            Age = 23,
+            Birthday = new DateTime(1991, 05, 20),
+            DepartamentName = "Administrare",
+            FirstName = "Ion",
+            LastName = "Frunze",
+            Id = 2,
+            PatronimicName = "Ionovici"
+        },
+        new Employee()
+        {
+            Age = 23,
+            Birthday = new DateTime(1991, 05, 20),
+            DepartamentName = "Auxiliar",
+            FirstName = "Iura",
+            LastName = "Streltov",
+            Id = 3,
+            PatronimicName = "Hersono"
+        },
+    };
+
+    public IActionResult Details(int id)
+    {
+        var employee = _employees.FirstOrDefault(x => x.Id == id);
+
+        if (employee == null) return NotFound();
+
+        return View(employee);
+    }
+    public IActionResult Index()
+    {
+        return View(_employees);
+    }
+}
