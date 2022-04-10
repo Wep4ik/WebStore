e553dca73b1e246b6aad09598b5c901e0c9670a9
Andrian <andrianpirlici@gmail.com>
2022-04-10 21:54:37 +0300

    Blog controller.

---
+++ WebStore/Controllers/BlogController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class BlogController : Controller
+    {
+        private static readonly List<Blog> _Blogs = new()
+        {
+            new Blog()
+            {
+                Title = "Girls 1 Pink T Shirt arrived in store",
+                Content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+                          "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
+                          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+            },
+            new Blog()
+            {
+                Title = "Girls 2 Pink T Shirt arrived in store",
+                Content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+                          "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
+                          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+            },
+            new Blog()
+            {
+                Title = "Girls 3 Pink T Shirt arrived in store",
+                Content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+                          "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
+                          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+            },
+        };
+
+        public IActionResult Index() => View(_Blogs);
+    }
+}
